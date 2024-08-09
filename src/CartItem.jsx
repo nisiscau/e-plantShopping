e6b92da5807786d35166df3333cc93961586469f
nisiscau <pierre.pochet@hec.edu>
2024-08-09 05:34:17 -0400

    9th

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addItem, removeItem, updateQuantity } from './CreatSlice';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const Cart = ({ onContinueShopping }) => {
